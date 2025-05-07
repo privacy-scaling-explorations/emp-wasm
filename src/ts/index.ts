b435e8f0a1d2c2631d1b7822fcc0d76878af45e7
Andrew Morris <voltrevo@gmail.com>
2025-05-07 16:10:52 +1000

    export channel<>byte conversion functions

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -2,3 +2,4 @@ export { default as secureMPC } from "./secureMPC.js";
 export { default as BufferedIO } from "./BufferedIO.js";
 export { default as BufferQueue } from "./BufferQueue.js";
 export { type IO } from "./types";
+export { channelFromByte, byteFromChannel } from "./utils.js";
